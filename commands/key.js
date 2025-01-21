f3c14c674891ccc09986532c1804bc40c8401ec9
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-21 12:44:41 +0530

    Check Command

--- commands/key.js
+++ commands/key.js
@@ -1,33 +1,50 @@
 const inquirer = require('inquirer')
 const colors = require('colors')
-const KeyManager = require('../lib/keyManager')
-const {isRequired} = require('../utils/validation')
-
+const KeyManager = require('../lib/KeyManager')
+const { isRequired } = require('../utils/validation')
 
 const key = {
-    async set() {
-        const keyManager = new KeyManager()
-        const input = await inquirer.prompt([
-            {
-                type: 'input',
-                name: 'key',
-                message: 'Enter API Key '.green + "https://nomics.com",
-                validate: isRequired
-            }
-        ])
+  async set() {
+    const keyManager = new KeyManager()
+    const input = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'key',
+        message: 'Enter API Key '.green + 'https://nomics.com',
+        validate: isRequired
+      }
+    ])
 
-        const key = keyManager.setKey(input.key)
+    const key = keyManager.setKey(input.key)
 
-        if(key){
-            console.log('API Key Set'.blue)
-        }
-    },
-    show() {
-        console.log('Hello from show')
-    },
-    remove() {
-        console.log('Hello from remove')
+    if (key) {
+      console.log('API Key Set'.blue)
     }
+  },
+  show() {
+    try {
+      const keyManager = new KeyManager()
+      const key = keyManager.getKey()
+
+      console.log('Current API Key: ', key.yellow)
+
+      return key
+    } catch (err) {
+      console.error(err.message.red)
+    }
+  },
+  remove() {
+    try {
+      const keyManager = new KeyManager()
+      keyManager.deleteKey()
+
+      console.log('Key Removed'.blue)
+
+      return
+    } catch (err) {
+      console.error(err.message.red)
+    }
+  }
 }
 
-module.exports = key;
+module.exports = key
