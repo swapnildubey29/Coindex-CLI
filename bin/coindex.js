f537ce95a0240d53e9d7bf9fe17e15b74bfaed32
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-21 11:32:13 +0530

    creating commands

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,4 +1,11 @@
 const program = require('commander')
+const pkg = require('../package.json')
+
+program
+.version(pkg.version)
+.command('key','Manage API key -- https://nomics.com')
+.parse(process.argv)
+
+
 
 console.log("Hello from coindex")
-console.log(process.argv)
