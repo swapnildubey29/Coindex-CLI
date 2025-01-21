790d14827b8e773b3b7ef93b32a4eb28e4fc131d
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-21 12:42:50 +0530

    Check API

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -1,19 +1,24 @@
-const { program } = require('commander');
-const key = require('../commands/key');
+const program = require('commander')
+const key = require('../commands/key')
 
 program
   .command('set')
-  .description('Set API key -- Get at https://nomics.com')
-  .action(key.set);
+  .description('Set API Key -- Get at https://nomics.com')
+  .action(key.set)
 
 program
   .command('show')
-  .description('Show API key')
-  .action(key.show);
+  .description('Show API Key')
+  .action(key.show)
 
 program
   .command('remove')
-  .description('Remove API key')
-  .action(key.remove);
+  .description('Remove API Key')
+  .action(key.remove)
 
-program.parse(process.argv);
+program.parse(process.argv)
+
+// If no args, output help
+if (!process.argv[2]) {
+  program.outputHelp()
+}
--- bin/coindex.js
+++ bin/coindex.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
-const { Command } = require('commander')
+const program = require('commander')
 const pkg = require('../package.json')
-
-const program = new Command()
 
 program
   .version(pkg.version)
-  .command('key', 'Manage API key -- https://nomics.com') // Define a subcommand
+  .command('key', 'Manage API Key -- https://nomics.com')
+  .command('check', 'Check Coin Price Info')
   .parse(process.argv)
-
-console.log("Hello from coindex")
